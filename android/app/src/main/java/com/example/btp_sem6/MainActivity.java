5333be4e67ca4b19d1168ad546113a6b40f215eb
shreyas-085 <shreyask085@gmail.com>
2022-01-19 16:12:05 +0530

    api linked

--- android/app/src/main/java/com/example/btp_sem6/MainActivity.java
+++ android/app/src/main/java/com/example/btp_sem6/MainActivity.java
@@ -19,15 +19,22 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import okhttp3.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void takePhoto() {
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_STORAGE);
         } else {
             dispatchTakePictureIntent();
@@ -63,47 +70,67 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void Init() {
-        selectedImg = findViewById(R.id.selectedImg);
-        takePhoto = findViewById(R.id.takePhoto);
-        uploadPhoto = findViewById(R.id.uploadPhoto);
-
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
-    }
-
     private void chooseImg() {
-        if(ContextCompat.checkSelfPermission(MainActivity.this,
-            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_UPLOAD_IMAGE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
         } else {
-            requestStoragePermission();
+            selectImg();
         }
-    }
-
-    private void requestStoragePermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_READ_STORAGE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_UPLOAD_IMAGE);
+        if (requestCode == REQUEST_READ_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectImg();
             }
         }
-        if(requestCode == REQUEST_CAMERA_STORAGE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_CAMERA_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 dispatchTakePictureIntent();
             }
         }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImgFile();
+            } catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.btp_sem6.provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_CAMERA_IMAGE);
+            }
+        }
+    }
+
+    private File createImgFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+        String fName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                fName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void selectImg() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_UPLOAD_IMAGE);
     }
 
     @Override
@@ -126,39 +153,6 @@ public class MainActivity extends AppCompatActivity {
 
     String currentPhotoPath;
 
-    private File createImgFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
-        String fName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                fName,
-                ".jpg",
-                storageDir
-        );
-
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImgFile();
-            } catch (IOException ex) {
-
-            }
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.btp_sem6.provider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_CAMERA_IMAGE);
-            }
-        }
-    }
-
     private void uploadPic(Uri imageUri) {
         final ProgressDialog pd = new ProgressDialog(this);
         pd.setTitle("Uploading Image");
@@ -166,19 +160,62 @@ public class MainActivity extends AppCompatActivity {
         final String randomKey = UUID.randomUUID().toString();
 
         StorageReference ref = storageReference.child("images/" + randomKey);
+        UploadTask uploadTask = ref.putFile(imageUri);
 
-        ref.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
-            Task<Uri> result = taskSnapshot.getMetadata().getReference().getDownloadUrl();
-            result.addOnSuccessListener(uri -> firebaseImgUri = uri.toString());
-            System.out.println(firebaseImgUri);
-            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+        uploadTask.continueWithTask(task -> {
+            if (!task.isSuccessful()) {
+                throw Objects.requireNonNull(task.getException());
+            }
+
+            return ref.getDownloadUrl();
+        }).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                firebaseImgUri = Objects.requireNonNull(task.getResult()).toString();
+                predict();
+            } else {
+                Toast.makeText(MainActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+            }
             pd.dismiss();
-        }).addOnFailureListener(e -> {
-            pd.dismiss();
-            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-        }).addOnProgressListener(snapshot -> {
-            double percent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
-            pd.setMessage(percent + "");
         });
+    }
+
+    private void predict(){
+        String Url = "http://65.2.128.63/predict";
+        System.out.println(firebaseImgUri);
+        post(Url, new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                System.out.println(Objects.requireNonNull(response.body()).string());
+            }
+        });
+    }
+
+    Call post(String url, Callback callback) {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n    \"img\": \"" + firebaseImgUri +"\"\r\n}");
+        Request request = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    private void Init() {
+        selectedImg = findViewById(R.id.selectedImg);
+        takePhoto = findViewById(R.id.takePhoto);
+        uploadPhoto = findViewById(R.id.uploadPhoto);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
     }
 }
