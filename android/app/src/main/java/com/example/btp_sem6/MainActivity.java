aeab28fc4c80b2681332c9e711de23edd2512d1d
shreyas-085 <shreyask085@gmail.com>
2022-03-31 20:42:12 +0530

    front end completed

--- android/app/src/main/java/com/example/btp_sem6/MainActivity.java
+++ android/app/src/main/java/com/example/btp_sem6/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.btp_sem6;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,22 +16,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
-import okhttp3.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,11 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_READ_STORAGE = 300;
     private static final int REQUEST_CAMERA_STORAGE = 400;
 
-    ImageView selectedImg;
-    Button takePhoto, uploadPhoto;
-    FirebaseStorage storage;
-    StorageReference storageReference;
-    String firebaseImgUri;
+    LinearLayout takePhoto, uploadPhoto;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,84 +120,23 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_UPLOAD_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             Uri imageUri = data.getData();
-            selectedImg.setImageURI(imageUri);
-            uploadPic(imageUri);
+            Intent intent = new Intent(this, ResultActivity.class);
+            intent.setData(imageUri);
+            startActivity(intent);
         }
 
         if (requestCode == REQUEST_CAMERA_IMAGE) {
             if (resultCode == RESULT_OK) {
                 File f = new File(currentPhotoPath);
-                selectedImg.setImageURI(Uri.fromFile(f));
-                uploadPic(Uri.fromFile(f));
+                Intent intent = new Intent(this, ResultActivity.class);
+                intent.setData(Uri.fromFile(f));
+                startActivity(intent);
             }
         }
     }
 
-    String currentPhotoPath;
-
-    private void uploadPic(Uri imageUri) {
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setTitle("Uploading Image");
-        pd.show();
-        final String randomKey = UUID.randomUUID().toString();
-
-        StorageReference ref = storageReference.child("images/" + randomKey);
-        UploadTask uploadTask = ref.putFile(imageUri);
-
-        uploadTask.continueWithTask(task -> {
-            if (!task.isSuccessful()) {
-                throw Objects.requireNonNull(task.getException());
-            }
-
-            return ref.getDownloadUrl();
-        }).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                firebaseImgUri = Objects.requireNonNull(task.getResult()).toString();
-                predict();
-            } else {
-                Toast.makeText(MainActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-            }
-            pd.dismiss();
-        });
-    }
-
-    private void predict(){
-        String Url = "http://65.2.128.63/predict";
-        System.out.println(firebaseImgUri);
-        post(Url, new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                System.out.println(Objects.requireNonNull(response.body()).string());
-            }
-        });
-    }
-
-    Call post(String url, Callback callback) {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n    \"img\": \"" + firebaseImgUri +"\"\r\n}");
-        Request request = new Request.Builder()
-                .url(url)
-                .method("POST", body)
-                .addHeader("Content-Type", "application/json")
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(callback);
-        return call;
-    }
-
     private void Init() {
-        selectedImg = findViewById(R.id.selectedImg);
         takePhoto = findViewById(R.id.takePhoto);
         uploadPhoto = findViewById(R.id.uploadPhoto);
-
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
-
     }
 }
